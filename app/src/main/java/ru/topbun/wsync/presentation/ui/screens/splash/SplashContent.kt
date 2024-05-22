9a110a76fd9d55d6dff6558d3a3cddf6d1126f6c
akimov1712 <akimov1712@mail.ru>
2024-05-22 15:57:17 +0300

    make SplashContent and add logo

--- app/src/main/java/ru/topbun/wsync/presentation/ui/screens/splash/SplashContent.kt
+++ app/src/main/java/ru/topbun/wsync/presentation/ui/screens/splash/SplashContent.kt
@@ -1,38 +1,65 @@
 package ru.topbun.wsync.presentation.ui.screens.splash
 
 import android.Manifest
-import android.util.Log
+import android.provider.CalendarContract.Colors
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
-import ru.topbun.wsync.utils.PermissionCheckedGranted
+import ru.topbun.wsync.R
 
 @Composable
 fun SplashContent(
     modifier: Modifier = Modifier,
     component: SplashComponent
 ) {
-    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) {
-        component.permissionCheckCompleted()
+    requestPermission(component)
+    Column(
+        modifier = modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.image_logo),
+            contentDescription = "Логотип",
+            modifier = Modifier.size(156.dp)
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        CircularProgressIndicator(color = MaterialTheme.colorScheme.primary)
     }
+}
+
+
+@Composable
+private fun requestPermission(component: SplashComponent) {
+    val launcher =
+        rememberLauncherForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+            component.permissionCheckCompleted()
+        }
     LaunchedEffect(Unit) {
         delay(300)
-        launcher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
-    }
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ){
-        CircularProgressIndicator()
+        launcher.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+            )
+        )
     }
 }
--- app/src/main/java/ru/topbun/wsync/presentation/ui/theme/Theme.kt
+++ app/src/main/java/ru/topbun/wsync/presentation/ui/theme/Theme.kt
@@ -2,45 +2,27 @@ package ru.topbun.wsync.presentation.ui.theme
 
 import android.app.Activity
 import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Primary,
 )
 
 @Composable
 fun WSyncTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
+    darkTheme: Boolean = false,
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -50,21 +32,20 @@ fun WSyncTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
+        darkTheme -> LightColorScheme
         else -> LightColorScheme
     }
     val view = LocalView.current
+    val window = (view.context as Activity).window
     if (!view.isInEditMode) {
         SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = Primary.toArgb()
         }
     }
 
     MaterialTheme(
+        content = content,
         colorScheme = colorScheme,
-        typography = Typography,
-        content = content
+        typography = Typography
     )
 }
